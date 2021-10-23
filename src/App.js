902752f29dfce398c68bd93b1871474ed73f325a
Wend Carlos <wendcarlos18@hotmail.com>
2021-10-23 20:21:59 -0300

    Project HTML Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react'
 
+import { 
+  Container, 
+  Image, 
+  ContainerItem, 
+  H1, 
+  InputLabel, 
+  Input, 
+  Button 
+} from './styles'
+
 const App = () => {
-  return <div>Hello World</div>
+  return (
+    <Container>
+      <Image/>
+      <ContainerItem>
+        <H1>Faça seu pedido</H1>
+
+        <InputLabel>Pedido</InputLabel>
+        <Input placeholder="Digite seu pedido"></Input>
+
+        <InputLabel>Nome</InputLabel>
+        <Input placeholder="Digite seu nome"></Input>
+
+        <Button>Novo Pedido</Button>
+
+      </ContainerItem>
+    </Container>
+  )
 }
 
 export default App
---
+++ src/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Container = styled.div `
+
+    background-color: #0A0A10;
+
+`
+
+export const Image = styled.img `
+
+`
+
+export const ContainerItem = styled.div `
+
+`
+
+export const H1 = styled.h1 `
+    font-size: 28px;
+    line-height: 33px;
+
+    text-align: center;
+
+    color: #FFFFFF;
+`
+
+export const InputLabel = styled.label `
+
+`
+
+export const Input = styled.input `
+
+`
+
+export const Button = styled.button `
+
+`
