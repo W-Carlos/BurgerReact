19cc2c06bdfeb6b751cb939305f0d9a0eded6542
Wend Carlos <wendcarlos18@hotmail.com>
2021-10-27 10:50:44 -0300

    Taking values from Inputs

--- src/App.js
+++ src/App.js
@@ -19,10 +19,21 @@ import {
 const App = () => {
 
   //const orders = []
-  const [orders, setOrders] = useState([])
+  const [orders, setOrders] = useState([]);
+  const [order, setOrder] = useState();
+  const [name, setName] = useState();
 
   function addNewOrder(){
-    setOrders([{id: Math.random(), order: "Cachorro-quente, Hamburguer, Coca-cola", clienteName: "Carlos"}])
+    setOrders([{id: Math.random(), order, name: name}])
+    console.log(name) 
+  }
+
+  function changeInputOrder(event){
+    setOrder(event.target.value)
+  }
+
+  function changeInputName(event){
+    setName(event.target.value)
   }
 
   return (
@@ -32,10 +43,10 @@ const App = () => {
         <H1>Faça seu pedido</H1>
 
         <InputLabel>Pedido</InputLabel>
-        <Input placeholder="Digite seu pedido"></Input>
+        <Input onChange={changeInputOrder} placeholder="Digite seu pedido"></Input>
 
         <InputLabel>Nome</InputLabel>
-        <Input placeholder="Digite seu nome"></Input>
+        <Input onChange={changeInputName} placeholder="Digite seu nome"></Input>
 
         <Button onClick={addNewOrder}>Novo Pedido</Button>
 
@@ -44,7 +55,7 @@ const App = () => {
             <Order key={order.id}>
               <Div>
                 <p>{order.order}</p>
-                <P>{order.clienteName}</P>
+                <P>{order.name}</P>
               </Div>
               <div>
                 <img src={Trash} alt="deletar"/>
