ff476348fdd253bb3ed60c0d66a8936a1f18a554
Wend Carlos <wendcarlos18@hotmail.com>
2021-11-02 10:23:57 -0300

    installed react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './containers/Home';
 import Order from './containers/Order';
-import GlobalStyle from './globalStyle';
+import GlobalStyle from './styles/globalStyle';
 
 ReactDOM.render(
     <>
